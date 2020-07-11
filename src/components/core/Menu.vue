cbb6b8de2097d6458fc1d2e3869255774dc22676
codewiz <c.chaiwat.ch@gmail.com>
2020-07-11 14:17:06 +0700

    Setup Login

--- src/components/core/Menu.vue
+++ src/components/core/Menu.vue
@@ -1,29 +1,39 @@
 <template>
   <v-navigation-drawer app permanent>
+
     <router-link to="/">
       <v-img src="@/assets/vue_display.jpg" width="100%"/>
     </router-link>
+
     <v-list shaped>
       <v-list-item-group v-model="selectedMenu" mandatory color="primary">
-        <v-list-item>menu1</v-list-item>
-        <v-list-item>menu2</v-list-item>
+        <v-list-item v-for="([icon, title, route], i) in menus" :key="i">
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-tile-title>
+              {{ title }}
+            </v-list-tile-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list-item-group>
     </v-list>
+
   </v-navigation-drawer>
 </template>
-
 
 <script>
   export default {
     name: "Menu",
     data () {
       return {
-        items: [
-          { title: 'Home', icon: 'mdi-home-city' },
-          { title: 'My Account', icon: 'mdi-account' },
-          { title: 'Users', icon: 'mdi-account-group-outline' },
-        ],
         selectedMenu: 0,
+        menus: [
+          ['mdi-apps-box', 'Stock', '/stock'],
+          ['mdi-chart-areaspline', 'Report', '/report'],
+          ['mdi-apple-safari', 'About', '/about'],
+        ]
       }
     },
     methods: {
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,27 @@
 <template>
   <v-container>
-    <h1>Login</h1>
+    <v-row class="justify-center">
+      <v-card style="width:400px;">
+        <v-img src="@/assets/login_header.jpg" height="200px"/>
+          <v-card-text>
+            <v-form>
+              <!-- Username -->
+              <v-text-field name="username" label="Username" id="username">
+              </v-text-field>
+
+              <!-- Password -->
+              <v-text-field name="password" label="Password" id="password">
+              </v-text-field>
+
+              <v-row class="justify-space-between px-3">
+                <v-btn color="primary">text</v-btn>
+                <v-btn>text</v-btn>
+              </v-row>
+
+            </v-form>
+          </v-card-text>
+      </v-card>
+    </v-row>
   </v-container>
 </template>
 

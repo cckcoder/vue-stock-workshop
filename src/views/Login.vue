5d107159ce4cfe44d01bfad2e5e019d97c20dd8c
codewiz <c.chaiwat.ch@gmail.com>
2020-07-26 17:15:30 +0700

    create Login and Register page
    Signed-off-by: codewiz <c.chaiwat.ch@gmail.com>

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,11 @@
   <v-container>
     <v-row class="justify-center">
       <v-card style="width:400px;">
-        <v-img src="@/assets/login_header.jpg" height="200px"/>
+        <v-img class="white--text align-end" src="@/assets/login_header.jpg" height="200px">
+          <v-card-title>
+            Login
+          </v-card-title>
+        </v-img>
           <v-card-text>
             <v-form>
               <!-- Username -->
@@ -10,12 +14,22 @@
               </v-text-field>
 
               <!-- Password -->
-              <v-text-field name="password" label="Password" id="password">
+              <v-text-field
+                name="password"
+                label="Password"
+                id="password"
+                :append-icon="isShowPassword ? 'visibility' : 'visibility_off'"
+                @click:append="isShowPassword = !isShowPassword"
+                :type="isShowPassword ? 'text' : 'password'"
+                counter
+              >
+
               </v-text-field>
 
-              <v-row class="justify-space-between px-3">
-                <v-btn color="primary">text</v-btn>
-                <v-btn>text</v-btn>
+              <!-- Button -->
+              <v-row class="justify-space-between px-3 pt-5">
+                <v-btn @click.prevent="$router.push('/register')">Register</v-btn>
+                <v-btn color="success">Login</v-btn>
               </v-row>
 
             </v-form>
@@ -27,7 +41,13 @@
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      isShowPassword: false
+    }
+  }
+
 }
 </script>
 
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,66 @@
 <template>
-  <h1>Register</h1>
+  <v-container>
+    <v-row class="justify-center">
+      <v-card style="width:400px;">
+        <v-img class="white--text align-end" src="@/assets/login_header.jpg" height="200px">
+          <v-card-title>
+            Register
+          </v-card-title>
+        </v-img>
+          <v-card-text>
+            <v-form @submit.prevent="submit">
+              <!-- Username -->
+              <v-text-field
+                name="username"
+                label="Username"
+                id="username"
+                v-model="account.username"
+                >
+              </v-text-field>
+
+              <!-- Password -->
+              <v-text-field
+                name="password"
+                v-model="account.password"
+                label="Password"
+                id="password"
+                :append-icon="isShowPassword ? 'visibility' : 'visibility_off'"
+                @click:append="isShowPassword = !isShowPassword"
+                :type="isShowPassword ? 'text' : 'password'"
+                counter
+              >
+
+              </v-text-field>
+
+              <v-row class="justify-space-between px-3 pt-5">
+                <v-btn @click.prevent="$router.back()">Cancel</v-btn>
+                <v-btn type="submit" color="success">Confirm</v-btn>
+              </v-row>
+
+            </v-form>
+          </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "Register"
+  name: "Register",
+  data() {
+    return {
+      isShowPassword: false,
+      account: {
+        username: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    submit() {
+      console.log(JSON.stringify(this.account))
+    }
+  }
 }
 </script>
 
